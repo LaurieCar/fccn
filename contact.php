23fc9cf7993fca1bcce4ec71315afcf66af8474b
LaurieCar <laurie.carriere26@gmail.com>
2024-01-26 18:59:46 +0100

    php

---
+++ contact.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+    
+    <link rel="stylesheet" href="CSS/header.css">
+    <link rel="stylesheet" href="CSS/contact.css">
+    <script src="./script.js" async></script>
+
+    <!--liens police d'écriture-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&family=Quicksand:wght@700&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+    <header id="header">
+        
+        <div id="logo">
+            <img src="images/logo.png" alt="logo" height="80vh" width="80vw">
+        </div>
+
+        <div class="burger">          
+            <button id="btnopen" class="menu-label">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 64 59" fill="none">
+                    <path d="M61 3.10204H3" stroke="#F5F5F5" stroke-width="5.9421" stroke-linecap="round"/>
+                    <path d="M61 29.2917H3" stroke="#F5F5F5" stroke-width="5.9421" stroke-linecap="round"/>
+                    <path d="M61 55.4814H3" stroke="#F5F5F5" stroke-width="5.9421" stroke-linecap="round"/>
+                </svg>
+            </button>
+        
+            <nav id="menu-nav">
+                <button id="btnClose">
+                    <img src="./images/btnclose.svg" alt="fermer" width="20" height="20">
+                </button>
+                <div id="flex">
+                <div class="club">
+                    <ul>
+                        <li class="menu-item">LE CLUB</li>
+                        <li class="menu-item"><a class="menu-link" href="#">A propos</a></li>
+                        <li class="menu-item"><a class="menu-link" href="#">Comité directeur</a></li>
+                        <li class="menu-item"><a class="menu-link" href="#">Nos partenaires</a></li>
+                        <li class="menu-item"><a class="menu-link" href="#">Devenir partenaire</a></li>
+                    </ul>
+                </div>
+                <div class="equipes">
+                    <ul>
+                        <li class="menu-item">LES EQUIPES</li>
+                        <li class="menu-item"><a class="menu-link" href="#">Ecole de Football</a></li>
+                        <li class="menu-item"><a class="menu-link" href="#">Pré-Formation</a></li>
+                        <li class="menu-item"><a class="menu-link" href="#">Séniors</a></li>
+                        <li class="menu-item"><a class="menu-link" href="#">Féminines</a></li>
+                    </ul>
+                </div>
+                <div class="infos">
+                    <ul>
+                        <li class="menu-item">INFOS PRATIQUES</li>
+                        <li class="menu-item"><a class="menu-link" href="#">Tarifs des licences</a></li>
+                        <li class="menu-item"><a class="menu-link" href="#">Planning des entraînements</a></li>
+                        <li class="menu-item"><a class="menu-link" href="./contact.php">Contact</a></li>
+                        <li class="menu-item"><a class="menu-link" href="./demandeEssai.html">Demande d'essai</a></li>
+                    </ul>
+                </div>
+                </div>
+        
+            </nav>        
+        </div>
+    
+
+        <a id="menu" href="./index.html">ACCUEIL</a>
+
+        <a id="actualites" href="./index.html">ACTUALITES</a>
+        <a id="boutique" href="https://app.grinta.eu/fc-canal-nord-1/products?collection=official">BOUTIQUE</a>
+       
+        <h2>Suivez-nous</h2>
+            
+            <div class="socialMedia">
+                <!--icone facebook-->
+                <a href="https://www.facebook.com/people/Football-Club-Canal-Nord/100057223128364/"><img src="./images/fb.svg" alt="logofb" class="facebook">
+                </a>
+                
+                <!--icone instagram-->
+                <a href="https://www.instagram.com/fc_canalnord/"><img src="./images/instagram.svg" alt="logoinsta" class="instagram">
+                </a>
+                <!--icone linkedin-->
+                <a href="https://www.linkedin.com/company/footballclubcanalnord/about/"><img src="./images/linkedin.svg" alt="logolinkedin" class="linkedin">
+                </a>
+            </div>              
+    </header>
+    
+    <section class="banniere">
+        <h1 class="banniere">SITE OFFICIEL</h1>
+        <img src="images/logo.png" alt="logo">
+        <h1 class="banniere">FC CANAL NORD</h1>
+    </section>
+
+    <main>
+        <div class="page">
+            <h1>Contact</h1>
+            <p>
+                Vous souhaitez plus de renseignements ? 
+                N'hésitez pas à nous contacter
+            </p>
+            <p>
+                Pour les demandes d'essai, référez-vous au formulaire sur la page <a class="lien" href="demandeEssai.html">demande d'essai</a>
+            </p>
+        </div>
+        
+
+        <h3>Contactez-nous :</h3>
+        <form action="./register.php" method="post">
+            
+            <label for="nom_contact">Saisir votre nom :</label>
+            <input name="nom_contact" type="text" placeholder="Nom" required="required"/>
+            <label for="prenom_contact">Saisir votre prénom :</label>
+            <input name="prenom_contact" type="text" placeholder="Prénom" required="required"/>
+            <label for="email_contact">Saisir votre email :</label>
+            <input name="email_contact" type="email" placeholder="E-mail" required="required"/>
+            <label for="telephone_contact">Saisir votre téléphone :</label>
+            <input name="telephone_contact" type="tel" placeholder="Téléphone" required="required"/>
+            <label for="message_contact" class="message">Saisir votre message :</label>
+            <textarea name="message_contact" type="message" placeholder="Message" required="required"></textarea>
+            <div class="button">
+                <button id="button" type="submit" name="submit">Envoyer</button>
+            </div>
+           
+        </form>
+
+        <div class="adresse">
+            <p>FC CANAL NORD <br>
+            13 chemin de Beldou <br>
+            31150 LESPINASSE</p>
+        </div>
+        <div class="adresse">
+            <p>fccanalnord@gmail.com<br>
+            06.11.22.33.44</p>
+        </div>
+        
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13719.27000460001!2d1.3744598833876194!3d43.708029399154015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12aea6094d0a23bf%3A0x7367926641946b2e!2s11%20Chem.%20de%20Beldou%2C%2031150%20Lespinasse!5e0!3m2!1sfr!2sfr!4v1704995999563!5m2!1sfr!2sfr" width="1200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </main>
+
+    <section id="sponsor">
+        <div class="sponsor">
+            <img class="img" src="images/sponsor.png" alt="logo sponsor">
+        </div>
+        <div class="sponsor">
+            <img class="img" src="images/sponsor2.png" alt="logo sponsor">
+        </div>
+        <div class="sponsor">
+            <img class="img" src="images/sponsor3.png" alt="logo sponsor">
+        </div>
+        
+    </section> 
+
+    <footer id="footer">
+        <ul class="footer">
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Mentions légales</a></li>
+            <li><a href="#">Documents officiels</a></li>
+            <li><a href="#">Gestion des cookies</a></li>
+        </ul>
+
+        <div class="socialMedia">
+                <!--icone facebook-->
+                <a href="https://www.facebook.com/people/Football-Club-Canal-Nord/100057223128364/"><img src="./images/fb.svg" alt="logofb" class="facebook">
+                </a>
+                
+                <!--icone instagram-->
+                <a href="https://www.instagram.com/fc_canalnord/"><img src="./images/instagram.svg" alt="logoinsta" class="instagram">
+                </a>
+                <!--icone linkedin-->
+                <a href="https://www.linkedin.com/company/footballclubcanalnord/about/"><img src="./images/linkedin.svg" alt="logolinkedin" class="linkedin">
+                </a>
+            </div>              
+    </footer>
+
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,48 @@
+<?php
+    // Import de la connexion à la BDD
+    include './connectBdd.php';
+
+    // Test si le bouton est cliqué
+    if(isset($_POST["submit"])){
+        //Test si tous les champs sont remplis
+        if(!empty($_POST["nom_contact"]) AND !empty($_POST["prenom_contact"]) AND !empty($_POST["email_contact"]) AND !empty($_POST["telephone_contact"]) AND !empty($_POST["message_contact"])){
+            // nettoyer les entrées du formulaire
+            $nom = cleanInput($_POST["nom_contact"]);
+            $prenom = cleanInput($_POST["prenom_contact"]);
+            $email = cleanInput($_POST["email_contact"]); 
+            $telephone = cleanInput($_POST["telephone_contact"]);
+            $message = cleanInput($_POST["message_contact"]);
+            // Ajouter le contact en BDD
+            ajouterContact($bdd,$nom,$prenom,$email,$telephone,$message);
+            // Afficher un message
+            header("Location:./remerciement.php");
+        }
+        else{
+            echo "Veuillez remplir tous les champs du formulaire";
+        }
+    }
+
+    // fonction pour ajouter un contact à la BDD
+    function ajouterContact($bdd,string $nom,string $prenom,string $email,string $telephone,string $message): void{
+        try {
+            // requete SQL
+            $requete = $bdd->prepare('INSERT INTO contact(nom_contact,prenom_contact,email_contact,telephone_contact,message_contact) VALUE(?,?,?,?,?)');
+            $requete->bindParam(1,$nom,PDO::PARAM_STR);
+            $requete->bindParam(2,$prenom,PDO::PARAM_STR);
+            $requete->bindParam(3,$email,PDO::PARAM_STR);
+            $requete->bindParam(4,$telephone,PDO::PARAM_STR);
+            $requete->bindParam(5,$message,PDO::PARAM_STR);
+            $requete->execute();
+        } 
+        // Bloc pour récupérer les erreurs
+        catch (\Exception $e) {
+            // Affichage de l'erreur
+            die("Error : ".$e->getMessage());
+        }
+    }
+
+    // function pour nettoyer les input du formulaire
+    function cleanInput(?string $value):?string{
+        return htmlspecialchars(strip_tags(trim($value)),ENT_NOQUOTES);
+    }
+?>
